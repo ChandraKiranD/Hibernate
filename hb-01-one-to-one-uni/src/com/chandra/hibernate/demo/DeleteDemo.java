f2e9d2e98fcd6d47d0fa82a197e5d503b81dbcec
Chandra Kiran Desaraju <88664447+ChandraKiranD@users.noreply.github.com>
2021-10-26 13:04:49 +0530

    Add files via upload

---
+++ hb-01-one-to-one-uni/src/com/chandra/hibernate/demo/DeleteDemo.java
@@ -0,0 +1,55 @@
+package com.chandra.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.chandra.hibernate.demo.entity.Instructor;
+import com.chandra.hibernate.demo.entity.InstructorDetail;
+
+public class DeleteDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+
+		// create a session
+
+		Session session = factory.getCurrentSession();
+
+		try {
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			//Get instructor by their primary key / id
+			int theId = 1;
+			
+			Instructor tempInstructor = session.get(Instructor.class, theId);
+			
+			System.out.println("Fount Instructor: " + tempInstructor);
+			
+			//delete the instructor
+			if(tempInstructor!= null) {
+				System.out.println("Deleting : " + tempInstructor);
+				
+				//Note: This will also delete the associated "details" object because of Cascadetype.ALL
+				session.delete(tempInstructor);
+			}
+			
+			// commit the transaction
+			session.getTransaction().commit();
+
+			System.out.println("Done!!");
+		} finally {
+			factory.close();
+		}
+
+	}
+
+}
---
+++ hb-02-one-to-one-bi/src/com/chandra/hibernate/demo/DeleteInstructorDetailDemo.java
@@ -0,0 +1,70 @@
+package com.chandra.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.chandra.hibernate.demo.entity.Instructor;
+import com.chandra.hibernate.demo.entity.InstructorDetail;
+
+public class DeleteInstructorDetailDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+
+		// create a session
+
+		Session session = factory.getCurrentSession();
+
+		try {
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			//get the instructor detail object
+			int theID = 3;
+			InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, theID);
+			//print the instructor detail 
+			
+			if(tempInstructorDetail != null) {
+			System.out.println(">>> Temp Isntructor Detail: " + tempInstructorDetail);
+			
+			//print the associated instructor
+			System.out.println(">>> The associated Instructor: "+tempInstructorDetail.getInstructor());
+			} else {
+				System.out.println(">>> Instructor Do Not Exist with ID : " + theID);
+			}
+			
+			
+			//Delete the instructor detail
+			System.out.println("::: Deleting tempInstructorDetail" + tempInstructorDetail);
+			
+			
+			//Remove the associated object reference
+			//break bi-directional reference
+			tempInstructorDetail.getInstructor().setInstructorDetail(null);
+			
+			session.delete(tempInstructorDetail);
+			
+			// commit the transaction
+			session.getTransaction().commit();
+
+			System.out.println("Done!!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			//Handle connection leak issue
+			session.close();
+			
+			factory.close();
+		}
+
+	}
+
+}
